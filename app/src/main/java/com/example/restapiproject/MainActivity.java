5ebf5087c7ab13d6b12939812d1d126272cf8252
Manali <you@example.com>
2021-05-21 20:30:26 -0400

    forgot fragment created

--- app/src/main/java/com/example/restapiproject/MainActivity.java
+++ app/src/main/java/com/example/restapiproject/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.restapiproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
