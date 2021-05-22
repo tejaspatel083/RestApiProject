6122350dc5746b78e9824ec0fbb37683c28cd807
TejasPatel <tjp083@gmail.com>
2021-05-22 00:47:52 -0400

    Login & Signup completed

--- app/src/main/java/com/example/restapiproject/LoginFragment.java
+++ app/src/main/java/com/example/restapiproject/LoginFragment.java
@@ -1,66 +1,48 @@
 package com.example.restapiproject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link LoginFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LoginFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    private Button btnLogin;
+    private TextView txtForgotPassword,txtSignup;
+    private EditText pwd,emailId;
+    private TextView v1,iv1;
+    private FirebaseAuth firebaseAuth;
+
 
     public LoginFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -69,42 +51,103 @@ public class LoginFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+        getActivity().setTitle("Login");
+
+        btnLogin = view.findViewById(R.id.MainLoginBtn);
+        txtForgotPassword = view.findViewById(R.id.MainForgotPassword);
+        txtSignup = view.findViewById(R.id.MainSignup);
+        pwd = view.findViewById(R.id.MainPassword);
+        emailId = view.findViewById(R.id.MainEmail);
+        v1 = view.findViewById(R.id.visible);
+        iv1 = view.findViewById(R.id.notvisible);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+
+        v1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pwd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                v1.setVisibility(View.INVISIBLE);
+                iv1.setVisibility(View.VISIBLE);
+            }
+        });
+        iv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                iv1.setVisibility(View.INVISIBLE);
+                v1.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+        View.OnClickListener navigate1 = Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_forgotPasswordFragment);
+        View.OnClickListener navigate2 = Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_signupFragment);
+
+        txtForgotPassword.setOnClickListener(navigate1);
+        txtSignup.setOnClickListener(navigate2);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (emailId.getText().toString().trim().length() == 0)
+                {
+                    emailId.setError("Email Id Required");
+
+                }
+                else if (pwd.getText().toString().trim().length() == 0)
+                {
+                    emailId.setError(null);
+                    pwd.setError("Password Required");
+
+                }
+                else
+                {
+                    emailId.setError(null);
+                    pwd.setError(null);
+
+                    String email = emailId.getText().toString().trim();
+                    String password = pwd.getText().toString().trim();
+
+
+                    firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful())
+                            {
+
+                                Toast.makeText(getActivity(), "Login SuccessFull", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getActivity(),HomeActivity.class);
+                                startActivity(intent);
+
+                            }
+                            else
+                            {
+
+                                Toast toast = Toast.makeText(getActivity(),"Enter Valid Email and Password",Toast.LENGTH_LONG);
+                                toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+                                toast.show();
+                            }
+
+                        }
+                    });
+
+                }
+            }
+        });
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
--- app/src/main/java/com/example/restapiproject/SignupFragment.java
+++ app/src/main/java/com/example/restapiproject/SignupFragment.java
@@ -4,107 +4,238 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.restapiproject.Models.UserInfo;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SignupFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SignupFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SignupFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    private Button btnSignup;
+    private TextView txtLogin;
+
+    private EditText user_pwd1,user_pwd2,user_name,user_email;
+    private TextView v1,v2,iv1,iv2;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore db;
+
+    private NavController navController;
+
+
 
     public SignupFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SignupFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SignupFragment newInstance(String param1, String param2) {
-        SignupFragment fragment = new SignupFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+        View view =  inflater.inflate(R.layout.fragment_signup,container,false);
+        getActivity().setTitle("Signup");
+
+        btnSignup = view.findViewById(R.id.CreateBtn);
+        txtLogin = view.findViewById(R.id.LoginText);
+
+        user_email = view.findViewById(R.id.CreateEmail);
+        user_name = view.findViewById(R.id.CreateName);
+        user_pwd1 = view.findViewById(R.id.CreatePassword);
+        user_pwd2 = view.findViewById(R.id.CreateRePassword);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        navController = Navigation.findNavController(getActivity(),R.id.Host_Fragment);
+
+
+        v1 = view.findViewById(R.id.visible1);
+        iv1 = view.findViewById(R.id.notvisible1);
+        v2 = view.findViewById(R.id.visible2);
+        iv2 = view.findViewById(R.id.notvisible2);
+
+        v1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user_pwd1.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                v1.setVisibility(View.INVISIBLE);
+                iv1.setVisibility(View.VISIBLE);
+            }
+        });
+        iv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user_pwd1.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                iv1.setVisibility(View.INVISIBLE);
+                v1.setVisibility(View.VISIBLE);
+            }
+        });
+        v2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user_pwd2.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                v2.setVisibility(View.INVISIBLE);
+                iv2.setVisibility(View.VISIBLE);
+            }
+        });
+        iv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user_pwd2.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                iv2.setVisibility(View.INVISIBLE);
+                v2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        View.OnClickListener navigate1 = Navigation.createNavigateOnClickListener(R.id.action_signupFragment_to_loginFragment);
+
+
+
+        txtLogin.setOnClickListener(navigate1);
+
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (user_name.getText().toString().trim().length()==0)
+                {
+                    user_name.setError("User Name Required");
+                }
+                else if(user_email.getText().toString().trim().length()==0)
+                {
+                    user_email.setError("Email ID Required");
+                }
+                else if (user_pwd1.getText().toString().trim().length()==0)
+                {
+                    user_pwd1.setError("Password Required");
+                }
+                else if (user_pwd2.getText().toString().trim().length()==0)
+                {
+                    user_pwd2.setError("Confirm Password Required");
+                }
+                else if (user_pwd1.getText().toString().trim().equals(user_pwd2.getText().toString().trim()))
+                {
+
+
+                    String email=user_email.getText().toString().trim();
+                    String password = user_pwd1.getText().toString().trim();
+
+
+
+
+                    firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful())
+                            {
+                                sendData();
+                            }
+
+                            else
+                            {
+
+                                if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+
+                                    Toast toast = Toast.makeText(getActivity(),"User with this email already exist.",Toast.LENGTH_LONG);
+                                    toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+                                    toast.show();
+                                }
+                                else
+                                {
+                                    Toast toast = Toast.makeText(getActivity(),"Check Internet Connection",Toast.LENGTH_LONG);
+                                    toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+                                    toast.show();
+                                }
+
+                            }
+                        }
+                    });
+
+
+
+                }
+                else
+                {
+                    Toast toast = Toast.makeText(getActivity(),"Password not matched",Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+                    toast.show();
+                }
+
+            }
+        });
+
+
+
+
+
+        return view;
+
+
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+    private void sendData()
+    {
+        String email1= user_email.getText().toString().trim();
+        String name = user_name.getText().toString().trim();
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+        UserInfo obj1 = new UserInfo(name,email1);
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+
+        db.collection("User Profile Information")
+                .document(firebaseAuth.getUid())
+                .set(obj1)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(getActivity().getApplicationContext(),"Registration Successful!",Toast.LENGTH_LONG).show();
+                            FirebaseAuth.getInstance().signOut();
+                            navController.navigate(R.id.action_signupFragment_to_loginFragment);
+
+                        }else
+                        {
+                            Toast.makeText(getActivity().getApplicationContext(),"FireStore Error!",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Toast.makeText(getActivity(), "Something Wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
